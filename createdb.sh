26c84d9c9ddf68491fe7788fa579829ec9597ff8
Max Resnick <max@silvio.bing>
2014-07-16 16:18:55 -0700

    Initial commit.

---
+++ createdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+create_mysql_db () {
+    while getopts ":d:m:u:" settings;
+    do
+        case $settings in
+          d)readonly DATABASEBASENAME=$OPTARG;;
+          m)readonly MYSQLUSER=$OPTARG;;
+          u)readonly DATABASEUSER=$OPTARG;;
+          \?)echo "Invalid arg" && exit 1;;
+        esac
+    done
+    mysql -u $MYSQLUSER -p -e "CREATE DATABASE $DATABASEBASENAME; GRANT ALL PRIVILEGES ON $DATABASEBASENAME.* to '$DATABASEUSER'@'localhost';";
+}
+create_mysql_db "$@";
