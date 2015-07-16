8f8d4f4b55ce3502011b8ad45fcc2ed507c3acfe
grumps <max@ofmax.li>
2015-07-15 22:55:21 -0700

    command complete, need getopts.

--- usr/local/bin/pydj.sh
+++ usr/local/bin/pydj.sh
@@ -1,14 +1,24 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash -xt
 
 pydj () {
-    local CURRENT_DIR=$(pwd)
+    _find_main_app() {
+        local readonly _main_app_settings_path=$(find . -maxdepth 2 -type d -name settings)
+        _main_app=$(dirname $_main_app_settings_path)
+        echo "${_main_app#./}"
+    }
+    # TODO GETOPTS
     if [ "$#" == 0 ]; then
         echo 'At least one argument is required'
         exit 1;
     fi
-    if [ -f $CURRENT_DIR/manage.py ]; then
-        # Need to find main app and autmagically use it.
-        # local MAIN_APP=
-        python manage.py $1 --settings=sayidowith_server.settings.$2
+    if [ -f ./manage.py ]; then
+        local readonly MAIN_APP=$(_find_main_app)
+        if [ "${#MAIN_APP}" == 0 ]; then
+            echo 'Main Django App with setting not found, exiting.';
+        else
+            python manage.py $1 --settings=$MAIN_APP.settings.$2
+        fi
+    else
+        echo 'pydj must be invoked with in the root directory of Django';
     fi
 }
